1cd35dd310a1eaa0abcdc51a97238b09684a1c6d
Philip <philipmjobe@gmail.com>
2021-11-23 09:21:19 -0500

    added vue1-filters and searchbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    <div v-for="movie in movies" v-bind:key="movie.id">
+    <div>
+      <button>Sort Alphabetically</button>
+    </div>
+    Search by name:
+    <input v-model="titleFilter" list="titles" />
+    <datalist id="titles">
+      <option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
+    </datalist>
+    <div v-for="movie in orderBy(filterBy(movies, titleFilter, `title`), `title`)" v-bind:key="movie.id">
       <h2>{{ movie.title }}</h2>
       <p>{{ movie.plot }}</p>
       <router-link v-bind:to="`/movies/${movie.id}`">
@@ -13,10 +21,13 @@
 <style></style>
 
 <script>
+import Vue2Filters from "vue2-filters";
 const axios = require("axios");
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
+      titleFilter: "",
       message: "Welcome to The Movies!",
       movies: [],
       newMovieParams: {},
